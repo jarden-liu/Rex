081b5d56dd12dbcce93f1507954008a9ce0bc3e6
jarden <jarden@jardendeiMac.lan>
2017-05-21 15:28:41 +0800

    update

--- app/index.js
+++ app/index.js
@@ -1,2 +1,12 @@
+/* 重置css */
+import "reset-css/reset.css";
+/* 重置css */
+
 // App entry, type your magical code  here.
 import Vue from "vue";
+import FastClick from "fastclick";
+
+import "flex.css/dist/flex.css";
+
+/* 添加FastClick到body */
+FastClick.attach(document.body);
